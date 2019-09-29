a44a172b0c34cd32f79cf7c423982f1f3a5a5b14
YOGESH <YOGESH@admin.mshome.net>
2019-09-29 08:27:06 -0700

    adsdf

--- practice project/src/pack1/class1.java
+++ practice project/src/pack1/class1.java
@@ -3,8 +3,9 @@ package pack1;
 public class class1 {
 	byte b=10;
 			public static void y1() {
-		System.out.println("b+100");
-		System.out.println("Hi ");
+		System.out.println("b+100");//1.0
+		System.out.println("Hi ");//1.1
+		System.out.println("Hello");//1.2
 	}
 public static void main(String[] args) {
 	y1();
